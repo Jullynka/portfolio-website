c17ea1c6c79fdf7d6aef4e5dc3ed873dff33305a
Katarina Roberts <katarinarobertsux@gmail.com>
2019-12-18 18:40:25 +0000

    Update scripts.js
    correcting javascript function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
-function menuToggle(){
-  var x=document.getElementById('myNavtoggle');
-  x.className+='responsive';
-}
-
-function myFunction() {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
